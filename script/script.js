4e554b8e660c713c991796a3b0efc97135e9800c
mujahidfisher <fishermujahid@gmail.com>
2023-05-29 12:19:27 +0200

    remove consol log

--- script/script.js
+++ script/script.js
@@ -4,15 +4,15 @@ document.querySelector("#calculate").addEventListener("click", function () {
   let output = document.querySelector("#result");
   //BMI calculation
   let result = (weight / (Math.pow(height, 2) / 10000)).toFixed(1);
-  console.log(result);
+//   console.log(result);
   if (result >= 30.0) {
     output.textContent = "overweight" + result
-    console.log("over weight" + result);
+    // console.log("over weight" + result);
   } else if (result >= 18.5) {
     output.textContent = "normal weight" + result
-    console.log("normal weight" + result);
+    // console.log("normal weight" + result);
   } else if (result < 18.5) {
     output.textContent = "underweight" + result
-    console.log("under weight" + result);
+    // console.log("under weight" + result);
   }
 });
