923b895e62ff82fc5750e7f568e9966e30db6332
mujahidfisher <fishermujahid@gmail.com>
2023-05-29 12:12:00 +0200

    finished

---
+++ script/script.js
@@ -0,0 +1,18 @@
+document.querySelector("#calculate").addEventListener("click", function () {
+  let height = parseFloat(document.querySelector("#height").value);
+  let weight = parseFloat(document.querySelector("#weight").value);
+  let output = document.querySelector("#result");
+  //BMI calculation
+  let result = (weight / (Math.pow(height, 2) / 10000)).toFixed(1);
+  console.log(result);
+  if (result >= 30.0) {
+    output.textContent = "overweight" + result
+    console.log("over weight" + result);
+  } else if (result >= 18.5) {
+    output.textContent = "normal weight" + result
+    console.log("normal weight" + result);
+  } else if (result < 18.5) {
+    output.textContent = "underweight" + result
+    console.log("under weight" + result);
+  }
+});
